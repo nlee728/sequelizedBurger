276e6d67a03aece42cf1c20600c38eab86984cad
unknown <38478896+nlee728@users.noreply.github.com>
2018-09-17 22:31:53 -0400

    config.json

--- models/burger.js
+++ models/burger.js
@@ -4,7 +4,11 @@ var sequelize = require ("../config/connection.js");
 //Create burger model to match database
 var Burger = sequelize.define("burger", {
   burger_name: Sequelize.STRING,
-  devoured: Sequelize.BOOLEAN
+  devoured: {  
+              type: Sequelize.BOOLEAN, 
+              allowNull: false, 
+              default: false 
+            }
 });
 
 //Sync with database
